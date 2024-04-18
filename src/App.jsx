4f267e1bd00826056c34692d2b09b0131458c5bb
roncy12 <jroncysn12@gmail.com>
2024-04-18 22:46:49 +0800

    setup gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,7 @@ import Portfolio from './components/portfolio/portfolio'
 import Testimonials from './components/testimonials/testimonials'
 import Contact from './components/contact/contact'
 import Footer from './components/footer/footer'
-import { Helmet } from 'react-helmet-async';
-// import P from './assets/js/particle.js'
-// import a from './assets/js/app.js'
+
 
 
 const App = () => {
@@ -25,7 +23,7 @@ const App = () => {
       <Testimonials />
       <Contact />
       <Footer />
-      {/* <div id="particles-js"></div> */}
+
 
 
     </div>
